d13de6297e93dec7ed29f28fa876d6181d73f946
Andrew <stealthgriz@gmail.com>
2016-08-15 18:43:00 +1200

    Server.js bug fixing
    Revision of server.js for bug fixing

--- server.js
+++ server.js
@@ -5,29 +5,24 @@
 
 // Package calls
 var express = require( "express" );
-var path = require( "path" );
 var bodyParser = require( "body-parser" );
-var mongodb = require( "mongodb" );
-var ObjectID = mongodb.ObjectID;
 
 // Express Definition
 var app = express();
 
-app.use(express.static(_dirname + "/public"));
-app.use( bodyParser.urlencoded( { extended: true } ) );
+//app.use( express.static(_dirname + "/public" ) );
 app.use( bodyParser.json() );
 
-var router = express.Router();
-app.use( '/api', router );
+var port = process.env.PORT || 8080;
 
-  // Initialize the server.
+
+
+/*  // Initialize the server.
   var server = app.listen( process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log( "App now running on port", port );
   });
-});
-
-
+});*/
 
 // Generic error handler used by all endpoints.
 function handleError( res, reason, message, code ) {
@@ -38,13 +33,20 @@ function handleError( res, reason, message, code ) {
 // TEST API ROUTE
 //=============================================================================================
 
-router.get('/', function(request, response) {
-	response.json({ msg-name:'Test Confirmation' msg-content:'Howdy pardner, Test sucessful! Good job pal!' });
+router.get( '/', function(request, response ) {
+	response.json( { message:'Howdy pardner, Test sucessful! Good job pal!' } );
 });
 
 // USER API ROUTES
 //=============================================================================================
 
 // TBA
+
+var router = express.Router();
+app.use( '/api', router );
+
+
+app.listen( port );
+console.log( 'App now running on port' + port );
 
 //=============================================================================================
