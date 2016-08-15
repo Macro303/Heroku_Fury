b58c035a1bbe472840dfd761b6e35225daf0976c
AndreaWolff <awolff@live.ca>
2016-08-16 11:23:23 +1200

    Implemented Users API
    Implemented Users API

--- server.js
+++ server.js
@@ -6,16 +6,16 @@
 // Package calls
 var express = require( "express" );
 var bodyParser = require( "body-parser" );
-
-// Express Definition
 var app = express();
 
-//app.use( express.static(_dirname + "/public" ) );
+//app.use( express.static(_dirname + "/public" ) ); <---- Unsure if necessary
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( bodyParser.json() );
 
 // Set up the port for listening
 var port = process.env.PORT || 8080;
+
+// Get an instance of the Express Router
 var router = express.Router();
 
 
@@ -28,7 +28,7 @@ function handleError( res, reason, message, code ) {
 // API routes
 //=============================================================================================
 
-
+// End-point for all routes
 router.use( function( request, response, next ) {
 	console.log('Loading........');
 	next();
@@ -36,15 +36,47 @@ router.use( function( request, response, next ) {
 
 // TEST API ROUTE
 router.get( '/', function(request, response ) {
+	// Test connection message
 	response.json( { 'message' :'Howdy pardner, Test sucessful! Good job pal!' } );
 });
 
 // USER API ROUTES
 //=============================================================================================
+
+// *****Main Route for /users*****
 router.route('/users')
 
+// get all users
 .get( function( request, response ) {
-	response.json( { 'First-name' :'Jimmy', 'Last-name':'Carter' } );
+	// Test function
+	response.json( { 'message':'get all users' } );
+});
+
+// create a user
+.post( function( request, response ) {
+	// Test function
+	response.json( { 'message':'create user' } );
+});
+
+// *****Main Route for /users:user_id******
+router.route('/users:user_id')
+
+// get a user for user_id
+.get( function( request, response ) {
+	// Test function
+	response.json( { 'message':'get user for user_id' } );
+});
+
+// update a user for user_id
+.put( function( request, response ) {
+	// Test function
+	response.json( { 'message':'update a user for user_id' } );
+});
+
+// delete a user for user_id
+.delete( function( request, response ) {
+	// Test function
+	response.json( { 'message':'delete user for user_id' } );
 });
 
 
