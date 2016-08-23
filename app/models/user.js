8ef26c6c905e8b28d7ac92caa3af292dc3605288
Andrew <stealthgriz@gmail.com>
2016-08-24 11:07:17 +1200

    Removed password requirement from user schema
    Removed password requirement from user schema

--- app/models/user.js
+++ app/models/user.js
@@ -6,8 +6,7 @@ var jwt = require( 'jsonwebtoken' );
 
 var userSchema = new mongoose.Schema({
 	username: { type: String, required:true, unique:true },
-	password: { type:String, required:true },
-	admin: { type:Boolean, required:true, default:false },
+	admin: { type:Boolean, default:false },
 	email:{ type:String, required:true },
 	created_at: { type:Date, default:Date.now },
 	hash: String,
