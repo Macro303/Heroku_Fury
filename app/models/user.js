f98a4a9c96bacdbbf901140e2c862523b33c099a
AndreaWolff <awolff@live.ca>
2016-08-23 17:14:57 +1200

    Fixed error in user.js
    Fixed error in user.js

--- app/models/user.js
+++ app/models/user.js
@@ -15,7 +15,7 @@ var userSchema = new mongoose.Schema({
 	updated_at: { type:Date, default:Date.now }
 });
 
-user.Schema.methods.setPassword = function( password ){
+userSchema.methods.setPassword = function( password ){
 	this.salt = crypto.randomBytes( 16 ).toString( 'hex' );
 	this.hash = crypto.pbkdf2Sync( password, this.salt, 1000, 64 ).toString( 'hex' );
 };
