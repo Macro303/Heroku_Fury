06fb8cc568e4d8d3210d3dfe47c8f75c9f652341
Andrew <stealthgriz@gmail.com>
2016-08-23 20:07:13 +1200

    Fixed missing ()
    Fixed missing ()

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose.connect( uristring, function( err, res ) {
 });
 
 gentleShutdown = function( msg, callback ){
-	mongoose.connection.close( function{
+	mongoose.connection.close( function(){
 		console.log( 'Mongoose disconnected through ' + msg );
 		callback();
 	});
