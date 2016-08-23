b434e54cd3508a656525135601deda378a6fd178
Andrew <stealthgriz@gmail.com>
2016-08-23 19:33:04 +1200

    Removed model call for testing
    Removed model call for testing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require( 'express' );
 var bodyParser = require( 'body-parser' );
 var mongoose = require( 'mongoose' );
 var passport = require( 'passport' );
-var User = mongoose.model( 'User' );
+//var User = mongoose.model( 'User' );
 
 require( './app/models/db.js' );
 
