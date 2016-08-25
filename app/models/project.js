a88169b99e93c0e528aef75871edf0988c3e9897
AndreaWolff <awolff@live.ca>
2016-08-26 10:44:13 +1200

    Added missing colon
    Added missing colon

--- app/models/project.js
+++ app/models/project.js
@@ -5,7 +5,7 @@ var mongoose = require( 'mongoose' );
 
 var projectSchema = new mongoose.Schema({
 	name: { type: String, required:true, unique:true },
-	description { type:String }
+	description: { type:String }
 });
 
 module.exports = mongoose.model( 'Project', projectSchema );
